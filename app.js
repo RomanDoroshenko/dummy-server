4d749dbb12c205e90b0ca176fe20963d216e2290
Your Name <you@example.com>
2018-05-25 08:23:04 +0000

    second

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mngiface = JSON.stringify(os.networkInterfaces())
   .match(/192\.168\.100\.\d{1,3}/)[0]
 
 // console.log(mngiface)
-
+app.use(express.static('static'));
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
