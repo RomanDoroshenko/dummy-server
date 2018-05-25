f5c9b89f76887227a2a241478a2a2fba0dae550a
Your Name <you@example.com>
2018-05-25 08:05:27 +0000

    initial

---
+++ app.js
@@ -0,0 +1,26 @@
+var os = require("os");
+var express = require('express');
+var app = express();
+
+
+const hostname = os.hostname();
+const mngiface = JSON.stringify(os.networkInterfaces())
+  .match(/192\.168\.100\.\d{1,3}/)[0]
+
+// console.log(mngiface)
+
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+
+  res.render('index', { 
+    hostname: hostname, 
+    mngiface: mngiface
+  });
+});
+
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
+});
+
+
